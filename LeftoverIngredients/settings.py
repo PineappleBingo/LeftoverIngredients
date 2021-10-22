9251595d430f9554a499bad58d1a6087f1b24d58
PineappleBingo <brownkpt60@gmail.com>
2021-10-22 16:29:45 -0400

    adding more apps

--- LeftoverIngredients/settings.py
+++ LeftoverIngredients/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'recipeComments.apps.RecipecommentsConfig',
+    'main.apps.MainConfig',
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
